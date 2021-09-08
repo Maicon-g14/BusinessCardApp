5fe018fd88eb8bb4aabd76c510dc17534868d132
Maicon-g14 <maicon_gabriel12@hotmail.com>
2021-09-08 15:45:00 -0300

    Project creation and Gradle configuration.

---
+++ app/src/main/java/com/myapp/businesscardapp/App.kt
@@ -0,0 +1,6 @@
+package com.myapp.businesscardapp
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/com/myapp/businesscardapp/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.myapp.businesscardapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.myapp.businesscardapp.R
+import com.myapp.businesscardapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
